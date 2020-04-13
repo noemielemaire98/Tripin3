04fd6bd851cb8b0f322626dc360ce429fbb4af28
Vithusan Sutharsan <sutharsanvithusan@gmail.com>
2020-04-13 15:30:41 +0100

    authentification

--- app/src/main/java/com/example/tripin/ui/profil/ProfilFragment.kt
+++ app/src/main/java/com/example/tripin/ui/profil/ProfilFragment.kt
@@ -1,12 +1,16 @@
 package com.example.tripin.ui.profil
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.tripin.RegistrationActivity
 import com.example.tripin.R
+import kotlinx.android.synthetic.main.fragment_profil.view.*
 
 class ProfilFragment : Fragment() {
 
@@ -21,6 +25,13 @@ class ProfilFragment : Fragment() {
                 ViewModelProviders.of(this).get(ProfilViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profil, container, false)
 
+        val button: Button = root.findViewById(R.id.auth_button_profil)
+        button.setOnClickListener {view ->
+            val intent = Intent(this.context, RegistrationActivity::class.java)
+            startActivity(intent)
+            true
+        }
         return root
     }
+
 }
