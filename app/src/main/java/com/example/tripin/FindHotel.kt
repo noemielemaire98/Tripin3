4ea7ac408683e4835109525527126bd1a122a4f3
Nzolo0 <55888232+Nzolo0@users.noreply.github.com>
2020-04-15 20:27:05 +0200

    Move AmadeusFragment to Activity

--- app/src/main/java/com/example/tripin/FindHotel.kt
+++ app/src/main/java/com/example/tripin/FindHotel.kt
@@ -2,11 +2,24 @@ package com.example.tripin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class FindHotel : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_hotel)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/tripin/ui/find/FindFragment.kt
+++ app/src/main/java/com/example/tripin/ui/find/FindFragment.kt
@@ -1,36 +1,13 @@
 package com.example.tripin.ui.find
 
-import android.annotation.SuppressLint
-import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import com.example.tripin.*
-import com.example.tripin.data.AppDatabase
-import com.example.tripin.data.FlightDao
-import com.example.tripin.model.Flight
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.android.synthetic.main.fragment_find.*
-import kotlinx.android.synthetic.main.fragment_find.view.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.jetbrains.anko.support.v4.alert
-import org.jetbrains.anko.yesButton
-import java.text.SimpleDateFormat
-import java.time.Duration
-import java.util.*
-import java.util.concurrent.TimeUnit
 
 
 /**
@@ -47,28 +24,25 @@ class FindFragment : Fragment() {
         val view: View = inflater.inflate(R.layout.fragment_find, container, false)
 
 
-        val bt_flight: Button = view.findViewById(R.id.bt_flight)
+        val btFlight: Button = view.findViewById(R.id.bt_flight)
 
-        bt_flight.setOnClickListener { view ->
+        btFlight.setOnClickListener {
             val intent = Intent(this.context, FindFlight::class.java)
             startActivity(intent)
-            true
         }
 
-        val bt_hotel: Button = view.findViewById(R.id.bt_hotel)
+        val btHotel: Button = view.findViewById(R.id.bt_hotel)
 
-        bt_hotel.setOnClickListener { view ->
+        btHotel.setOnClickListener {
             val intent = Intent(this.context, FindHotel::class.java)
             startActivity(intent)
-            true
         }
 
-        val bt_activites: Button = view.findViewById(R.id.bt_activities)
+        val btActivities: Button = view.findViewById(R.id.bt_activities)
 
-        bt_activites.setOnClickListener { view ->
+        btActivities.setOnClickListener {
             val intent = Intent(this.context, FindActivites::class.java)
             startActivity(intent)
-            true
         }
 
         return view
